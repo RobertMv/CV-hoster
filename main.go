ff29f6284d7e75253502d8fc4995cf6037fb962f
RobertMv <robert.mulyukov@gmail.com>
2022-09-16 20:03:09 +0300

    little update

--- main.go
+++ main.go
@@ -3,38 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
-type ViewData struct {
-	Title   string
-	Message string
-}
-
 func main() {
-	arrowDown := '↓'
-	smile := '🙃'
-
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		data := ViewData{
-			Title: "Robert's CV",
-			Message: fmt.Sprintf("Hi %c! You can choose the language of my CV bellow %c",
-				smile, arrowDown),
-		}
-		tmpl, _ := template.ParseFiles("index.html")
-		if err := tmpl.Execute(c.Writer, data); err != nil {
-			log.Fatal(err)
-		}
-	})
 	router.GET("/cv/en", func(c *gin.Context) {
 		c.File("Robert Mulyukov.pdf")
-	})
-	router.GET("/cv/ru", func(c *gin.Context) {
-		c.File("Мулюков Роберт.pdf")
 	})
 
 	fmt.Printf("Starting server at port %s\n", os.Getenv("PORT"))
